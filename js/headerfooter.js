09bb6ff64538bea8d5c8fdc1b0715c5315e368d9
Anand Upender <anand.upender@gmail.com>
2019-09-11 18:19:50 +0200

    performance changes

--- js/headerfooter.js
+++ js/headerfooter.js
@@ -1,15 +1,17 @@
 var headContent = document.getElementsByTagName('head')[0];
 
-var googleAnalyticsFirst ="https://www.googletagmanager.com/gtag/js?id=UA-130786913-1"
+var googleAnalyticsFirst = "https://www.googletagmanager.com/gtag/js?id=UA-130786913-1"
 
 var googleAnalyticsInner =
-`
+  `
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
   gtag('config', 'UA-130786913-1');
 `
 var gFirst = document.createElement('script');
+
+
 gFirst.src = googleAnalyticsFirst;
 
 var gInner = document.createElement('script');
@@ -19,14 +21,16 @@ s.parentNode.insertBefore(gFirst, s);
 s.parentNode.insertBefore(gInner, s);
 
 headContent.innerHTML += `
-  <link rel="apple-touch-icon-precomposed" sizes="57x57" href="images/favicons/apple-touch-icon-57x57.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/favicons/apple-touch-icon-114x114.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/favicons/apple-touch-icon-72x72.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/favicons/apple-touch-icon-144x144.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="60x60" href="images/favicons/apple-touch-icon-60x60.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="120x120" href="images/favicons/apple-touch-icon-120x120.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="76x76" href="images/favicons/apple-touch-icon-76x76.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="152x152" href="images/favicons/apple-touch-icon-152x152.png" />
+  <meta name="description" content="Anand is a creative strategist and digital prototyper with a knack for moving teams forward through humble and thoughtful design directions." />
+  <meta name="keywords" content="stanford, anand upender, web design, digital design, designer, community creator" />
+  <link rel="apple-touch-icon" sizes="57x57" href="images/favicons/apple-touch-icon-57x57.png" />
+  <link rel="apple-touch-icon" sizes="114x114" href="images/favicons/apple-touch-icon-114x114.png" />
+  <link rel="apple-touch-icon" sizes="72x72" href="images/favicons/apple-touch-icon-72x72.png" />
+  <link rel="apple-touch-icon" sizes="144x144" href="images/favicons/apple-touch-icon-144x144.png" />
+  <link rel="apple-touch-icon" sizes="60x60" href="images/favicons/apple-touch-icon-60x60.png" />
+  <link rel="apple-touch-icon" sizes="120x120" href="images/favicons/apple-touch-icon-120x120.png" />
+  <link rel="apple-touch-icon" sizes="76x76" href="images/favicons/apple-touch-icon-76x76.png" />
+  <link rel="apple-touch-icon" sizes="152x152" href="images/favicons/apple-touch-icon-152x152.png" />
   <link rel="icon" type="image/png" href="images/favicons/favicon-196x196.png" sizes="196x196" />
   <link rel="icon" type="image/png" href="images/favicons/favicon-96x96.png" sizes="96x96" />
   <link rel="icon" type="image/png" href="images/favicons/favicon-32x32.png" sizes="32x32" />
@@ -47,47 +51,49 @@ var nav = document.getElementById("nav");
 nav.innerHTML += `
   <div id="top-rectangle"></div>
   <div id="logo">
-    <a class="name menu-buttons animated fadeInDown" href="index.html"> Anand Upender</a>
+    <a class="name menu-buttons animated fadeInDown" href="index.html" title="Anand Upender Logo"> Anand Upender</a>
 
   </div>
-  <div id="menu">
-    <a href="index.html#scrollTo" class="menu-buttons animated fadeInDown" id="work"> Work </a>
-    <a href="cooking.html" class="menu-buttons only-mobile animated fadeInDown" style="padding-top:0px;">Food</a>
-    <a href="https://stanfordmobility.com" target="_blank" class="menu-buttons only-mobile animated fadeInDown">Mobility</a>
-    <a href="place.html" class="menu-buttons only-mobile animated fadeInDown">Spaces</a>
-    <div class="dropdown">
-      <a href="cooking.html" class="menu-buttons animated fadeInDown" id="research">Research &#x25be;</a>
-      <div class="dropdown-content">
-        <a href="cooking.html" class="menu-buttons" style="padding-top:0px;">Food</a>
-        <a href="https://stanfordmobility.com" target="_blank" class="menu-buttons">Mobility</a>
-        <a href="place.html" class="menu-buttons">Spaces</a>
-        <!-- a href="#" class="menu-buttons">Web</a>
-        <a href="http://erinmacd.stanford.edu/?page_id=352" target="_blank" class="menu-buttons">Research Metaphors</a> --!>
-      </div>
+  <nav>
+    <ul id="menu">
+      <li><a href="index.html#scrollTo" class="menu-buttons animated fadeInDown" id="work" title="Anand's Work"> Work </a></li>
+      <li><a href="cooking.html" class="menu-buttons only-mobile animated fadeInDown" style="padding-top:0px;" title="Anand's Food Research">Food</a></li>
+      <li><a href="https://stanfordmobility.com" target="_blank" rel="noopener noreferrer" class="menu-buttons only-mobile animated fadeInDown" title="Anand's Mobility Research">Mobility</a></li>
+      <li><a href="place.html" class="menu-buttons only-mobile animated fadeInDown" title="Anand's Spaces Research">Spaces</a></li>
+      <li><div class="dropdown">
+        <a href="cooking.html" class="menu-buttons animated fadeInDown" id="research" title="Anand's Research">Research &#x25be;</a>
+        <div class="dropdown-content">
+          <a href="cooking.html" class="menu-buttons" style="padding-top:0px;" title="Anand's Food Research">Food</a>
+          <a href="https://stanfordmobility.com" target="_blank" rel="noopener noreferrer" class="menu-buttons" title="Anand's Mobility Research">Mobility</a>
+          <a href="place.html" class="menu-buttons" title="Anand's Spaces Research">Spaces</a>
+          <!-- a href="#" class="menu-buttons">Web</a>
+          <a href="http://erinmacd.stanford.edu/?page_id=352" target="_blank" class="menu-buttons" title="Anand's Metaphor Research">Research Metaphors</a> --!>
+        </div>
+      </div></li>
+      <li><a href="experiments.html" class="menu-buttons animated fadeInDown" id="tinkering" title="Anand's Experiments"> Tinkering </a></li>
+      <li><a href="about.html" class="menu-buttons animated fadeInDown no-margin-right" id="about" title="About Anand">About</a></li>
     </div>
-    <a href="experiments.html" class="menu-buttons animated fadeInDown" id="tinkering"> Tinkering </a>
-    <a href="about.html" class="menu-buttons animated fadeInDown no-margin-right" id="about">About</a>
-  </div>
-  <a href="javascript:void(0);" class="icon animated fadeInDown" onclick="responsiveMenu()">
+  </nav>
+  <a href="javascript:void(0);" class="icon animated fadeInDown" onclick="responsiveMenu()" title="hamburger menu for mobile">
     <i class="fa fa-bars"></i>
   </a>
 `;
 
 function responsiveMenu() {
-    var x = document.getElementById("nav");
-    if (x.className === "topNav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topNav";
-    }
+  var x = document.getElementById("nav");
+  if (x.className === "topNav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topNav";
+  }
 }
 
 var footer = document.getElementById("footer");
 footer.innerHTML += `
   <div id="bottom-rectangle"></div>
   <a class="bottom-button no-margin-left" target="_blank" href="mailto:anandx@stanford.edu"><div class="animated fadeInUp delay-1s">Email</div></a>
-  <a class="bottom-button" target="_blank" href="https://drive.google.com/file/d/1MZqF19jbrnSfgC-fTu9E4IYMhKm9M0fJ/view?usp=sharing"><div class="animated fadeInUp delay-1s">Resume</div></a>
-  <div id="bottom-middle-button" class="bottom-button bottom-button-large animated fadeInUp delay-1s">Made from scratch with&nbsp;<a class="heart" href="https://www.pantone.com/color-intelligence/color-of-the-year/color-of-the-year-2019" target="_blank">&#9829;</a></div>
-  <a class="bottom-button" target="_blank" href="https://www.github.com/anandupender"><div class="animated fadeInUp delay-1s">Github</div></a>
+  <a class="bottom-button" target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1MZqF19jbrnSfgC-fTu9E4IYMhKm9M0fJ/view?usp=sharing"><div class="animated fadeInUp delay-1s">Resume</div></a>
+  <div id="bottom-middle-button" class="bottom-button bottom-button-large animated fadeInUp delay-1s">Made from scratch with&nbsp;<a class="heart" href="https://www.pantone.com/color-intelligence/color-of-the-year/color-of-the-year-2019" target="_blank" rel="noopener noreferrer">&#9829;</a></div>
+  <a class="bottom-button" target="_blank" rel="noopener noreferrer" href="https://www.github.com/anandupender"><div class="animated fadeInUp delay-1s">Github</div></a>
   <a class="bottom-button no-margin-right" href="instagram.html"><div class="animated fadeInUp delay-1s" id="insta-button">Instagram</div></a>
 `;
--- js/particleInit.js
+++ js/particleInit.js
@@ -1,7 +1,7 @@
 particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 300,
+      "value": 250,
       "density": {
         "enable": true,
         "value_area": 800
