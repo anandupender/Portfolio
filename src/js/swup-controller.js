da456d855ed671309cff611a26b3616488db7e35
anandupender <anand.upender@gmail.com>
2021-03-22 08:15:03 -0700

    changed SEO stuff

--- src/js/swup-controller.js
+++ src/js/swup-controller.js
@@ -1,3 +1,40 @@
+let face = `
+,,,,,,,,,,,,,,,,,,,,,,,,,::::,::::::::~~
+,,,,.,,,,,,,,.,,$I88DI$.,,:,,:::::::::~:
+,,,,,.,..,.,ZID8MDNNMNMOMMM?,,,:::::::::
+,,,,,,,,?O7MMMMMNMMMNMMMMMMMMMD:::::::~~
+,,..,...+DNNMMNMNNMMNMMMMDMDM8MM~::::~~:
+,,,..,I8DDMMNMMMMMMMMMMMMMNNMMDMM:::::::
+.,..,.OMMMMMMMMMNMMMNNMMMMDMMMMMN+::::::
+...,.OMMMMMMDO8ZOZZ$$$7$ODMMNMDMMD~:::::
+....,DMMMNOZ$$I?7777III+??I8MMMMMM7~~:::
+.....DNMMO$77I???III7I?+???I7$MMMMN:::::
+....NNNNN$77I?II??I7I+?I7?+?7$DMMMN:~~::
+....$MNNN7IIIIII?I?II=+?I??OZ$OMMM7~~~~:
+.....MMMM7OM8NN8O8Z7??ODNMNDON7MMM?::~~~
+,,.,.7NMDMNI??$ZZDDZZD888OZ$I7D=MM?:::~~
+,,,,,:M+MD+77MMOD$MOIM$NOMNMO8MMMM7~~~~~
+,,,,,=MMN?=+I$OODIM+?8$$MOZ$$7IMMD=~~~~~
+:::,,:D7MIM=+?7IIM7?I7N$7?I=~?7M??~~~~==
+::~::::88?+~+?77III:?=+=N8Z?~+7$N~~~~~~=
+:~:~::,ZDM?++78II8$+$MM$?8Z7II$7I~~~==~=
+:::::::,7O777D8ZO8NMNND8O8O8ZZZ$:~~~=~~+
+:~::::::,,$$IOO8ZZZZ88DDDMNOZOZ::~~~==+=
+:::~~:~::.$$?878MD==?~?Z87ZZO88::~~~====
+~~~~:::,,,M7I77I$=+??+I7I7ZZZ8=:~~~~~==+
+~~~~::::,:,O$I$77OMMNNZ7I?$ZOI:::~~~===+
+~~~:::::,,,?N$7I$$7ZZ7??IZO8I::~~~~===++
+~~:::::::,,Z$$O87O$OOO8DN8ZZ:::~~~~====?
+~~~::::, .O$7II$NDNNMMDN8OZZZ8.~=====+=+
+~~:~.. ...ZZ777I7$8DDDDO$ZZZZ$Z...=+++++
+~.. .  .  I$$7777I7ZO8ZZZZOZZ7D.    .??+
+..  .. .. .O$7$Z77IIZZOOOOOOOZN.      ..
+..  .. .    .$Z$$$$$O8OO8OZOZM.         
+.. .,. ..    ..D$ZZOOOOOOOZZ:           
+ .,.,.  .       .IDD88OOON...           
+.:.                  ..                 
+`;
+
 const swup = new Swup({
     plugins: [
         new SwupScrollPlugin({
@@ -35,9 +72,9 @@ function init(){
         });  
     }
     if(document.querySelector("#more") !== null){
-        function openMore(){
+        document.querySelector("#read-more").addEventListener("click", function(){
             document.querySelector("#more").style.display = "block";
-        }
+        });
     }
     if(document.querySelector(".tilt") !== null){
         VanillaTilt.init(document.querySelectorAll(".tilt"), {
@@ -45,9 +82,13 @@ function init(){
             scale:1.05
         });
     }
+
 }
 
 init();
+
+// fun console log
+console.log(face);
 
 // this event runs for every page view after initial load
 swup.on('contentReplaced', init);
